12cca43b9a0c11e7c66eaeb092de2d2b4ab74bdc
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-08-11 23:03:29 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,9 @@
+var a=10
+var b=20
+var c=a+b
+console.log("A value is "+a)
+console.log("B value is "+b)
+console.log("C value is ",+c)
+
+console.log(`a value is ${a} b value is ${b} c value is ${a+b }`
+)
