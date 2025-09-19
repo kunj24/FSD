97d59fe851f2180b2b0b56196d38a006dbcf8502
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-19 18:34:17 +0530

    Delete demo.js

--- demo.js
+++
@@ -1,9 +0,0 @@
-var a=10
-var b=20
-var c=a+b
-console.log("A value is "+a)
-console.log("B value is "+b)
-console.log("C value is ",+c)
-
-console.log(`a value is ${a} b value is ${b} c value is ${a+b }`
-)
